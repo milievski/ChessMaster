a261a3cc826117a603ffb6110abce3d6591a1ca2
milievski <m.ilievski@uleth.ca>
2016-02-08 20:49:25 -0700

    Start of QT

--- source/QTMenu/MainMenuQT.h
+++ source/QTMenu/MainMenuQT.h
@@ -23,42 +23,12 @@ class MainMenuQT: public QMainWindow
     MainMenuQT();
     
   private slots:
-  void updateTime();
+
 
 
   private:
 
-    //Time Progress Bars
-    QProgressBar *hourBar;
-    QProgressBar *minuteBar;
-    QProgressBar *secondBar;
-
-    //Date Progress Bars
-    QProgressBar *dayBar;
-    QProgressBar *monthBar;
     
-    //Alarm Button
-    QPushButton *alarmOne;
-    QPushButton *alarmTwo;
-    QPushButton *alarmThree;
-    QPushButton *alarmFour;
-
-    //Other Buttons
-    QPushButton *nightDayB;
-    QPushButton *yearB;
-    QPushButton *quitB;
-
-    //Labels
-    QLabel *hourL;
-    QLabel *minuteL;
-    QLabel *secondL;
-    QLabel *timeOfDayL;
-    QLabel *dayL;
-    QLabel *monthL;
-    QLabel *yearL;
-
-    //Time and Timer
-    QTimer *updateT;
 
 
 
