343a532af11232e4090084d54798688847d3e8db
milievski <m.ilievski@uleth.ca>
2016-02-19 18:25:44 -0700

    Change rule promotion

--- source/Rules/Promotion.cc
+++ source/Rules/Promotion.cc
@@ -13,14 +13,14 @@ int Promotion::validMove(vector< vector < Piece* > > &b, int ix, int iy, int dx,
    if(b[ix][iy]->getColor() == 0)
    {
       //checks if the final location is at the final square
-      if(dx == 7)
+      if(dx == 0)
           return 4;
    }
    //Black
    else if(b[ix][iy]->getColor() == 1)
    {
       //checks if the final location is at the final square return 4 if this is true
-      if(dx == 0)
+      if(dx == 7)
 	 return 4;
    }
    
