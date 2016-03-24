2fb0f7ed0e9018d342e254debdd510ed01dfb6c9
michael-wilson1993 <wilsonm_1993@otmail.com>
2016-03-24 15:22:49 -0600

    added userInfo class

---
+++ source/userInfo.cpp
@@ -0,0 +1,3 @@
+#include "userInfo.h"
+
+extern userInfo information;
---
+++ source/userInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+
+
+
+class userInfo
+{
+public:
+	std::string player1;
+	std::string player2;
+
+};
+
